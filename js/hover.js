dafd155821378738f357f5f82aad44670cc73abd
Kirill <scerep529@gmail.com>
2023-04-23 15:01:13 +0600

    my first commit

---
+++ js/hover.js
@@ -0,0 +1,17 @@
+const line = document.querySelector('#tel');
+
+// document.querySelector('.header__container-tel').onmouseover = () => {
+//     line.classList.add('border-bot');
+//     line.classList.remove('border-bot');
+//     line.classList.remove('hidden');
+//     line.classList.add('visible');
+// };
+
+// document.querySelector('.header__container-tel').onmouseout = () => {
+//     line.classList.remove('border-bot');
+//     line.classList.add('hidden');
+
+
+// };
+
+
