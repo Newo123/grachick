01370873777171c8d2f8ab3dd2371ba787fa614e
Kirill <scerep529@gmail.com>
2023-05-03 10:29:34 +0600

    update menu thrid

--- js/burger.js
+++ js/burger.js
@@ -4,21 +4,88 @@ const toggleMenuBtn = document.querySelector('.burger');
 const toggleMenuBtnSpan = document.querySelectorAll('.burger span');
 const menu = document.querySelector('.mobile-menu');
 const menuShadow = document.querySelector('.menu-shadow');
+const secondMenuItem = document.getElementsByClassName('mobile-menu__list__item-second__item');
+const thridMenuItem = document.getElementsByClassName('mobile-menu__list__item-second__thrid-item');
+const secondMenu = document.querySelector('.mobile-menu__list__item-second');
+const fourthMenu = document.querySelectorAll('.mobile-menu__list__item-second__thrid-item__fourth');
 
 item.addEventListener('click', () => {
 
-    if (item.children[2].style.transform !== 'rotate(90deg)') {
-        item.children[2].style.transform = 'rotate(90deg)';
-        item.children[2].classList.remove('arrow-white');
-        item.children[2].classList.add('gold-arrow');
+    if (item.children[1].style.transform !== 'rotate(90deg)') {
+        item.children[1].style.transform = 'rotate(90deg)';
+        item.children[1].classList.remove('arrow-white');
+        item.children[1].classList.add('gold-arrow');
+        setTimeout(() => {
+            secondMenu.style.overflow = 'visible';
+        }, 500);
+        secondMenu.style.maxHeight = '100%';
         link.style.color = '#BB8E5D';
     } else {
-        item.children[2].style.transform = 'rotate(0)';
-        item.children[2].classList.remove('gold-arrow');
-        item.children[2].classList.add('arrow-white');
+        item.children[1].style.transform = 'rotate(0)'; 
+        item.children[1].classList.remove('gold-arrow');
+        item.children[1].classList.add('arrow-white');
+        secondMenu.style.maxHeight = '0';
+        secondMenu.style.overflow = 'hidden';
         link.style.color = '#ffffff';
     }
 });
+
+const openCloseMenu = (item, e) => {
+    // e.stopPorogation();
+    // e.preventDefault();
+
+    if (item.children[1].style.transform !== 'rotate(90deg)') {
+
+        for (let i = 0; i < secondMenuItem.length; i++) {
+            if (secondMenuItem[i] !== item) {
+                secondMenuItem[i].style.maxHeight = '0';
+                secondMenuItem[i].style.overflow = 'hidden';
+                secondMenuItem[i].style.paddingBottom = '0';
+            }
+        }
+
+        setTimeout(() => {
+
+            item.nextElementSibling.style.overflow = 'visible';
+        }, 500);
+
+        item.nextElementSibling.style.maxHeight = '100%';
+        item.nextElementSibling.style.paddingBottom = '20px';
+        item.children[1].style.transform = 'rotate(90deg)';
+        item.children[1].classList.remove('arrow-white');
+        item.children[1].classList.add('gold-arrow');
+        item.children[0].style.color = '#BB8E5D';
+
+    } else {
+
+        setTimeout(() => {
+            for (let i = 0; i < secondMenuItem.length; i++) {
+                if (secondMenuItem[i] !== item) {
+                    secondMenuItem[i].style.maxHeight = '100%';
+                    secondMenuItem[i].style.overflow = 'visible';
+                    secondMenuItem[i].style.paddingBottom = '20px';
+                }
+    
+            }
+        }, 300);
+
+        item.nextElementSibling.style.overflow = 'hidden';
+        item.nextElementSibling.style.maxHeight = '0';
+        item.nextElementSibling.style.paddingBottom = '0';
+        item.children[1].style.transform = 'rotate(0)';
+        item.children[1].classList.add('arrow-white');
+        item.children[1].classList.remove('gold-arrow');
+        item.children[0].style.color = '#ffffff';
+    }
+}
+
+for (let i = 0; i < secondMenuItem.length; i++) {
+    secondMenuItem[i].children[1].addEventListener('click', () => openCloseMenu(secondMenuItem[i]));
+}
+
+for (let i = 0; i < thridMenuItem.length; i++) {
+    thridMenuItem[i].children[1].addEventListener('click', () => openCloseMenu(secondMenuItem[i]));
+}
 
 const toggleMenu = () => {
     console.log(menu.style.transform)
@@ -29,6 +96,7 @@ const toggleMenu = () => {
         toggleMenuBtnSpan[0].style.transform = 'translate(0, 100%) rotate(-45deg)';
         toggleMenuBtnSpan[1].style.transform = 'scale(0)'
         toggleMenuBtnSpan[2].style.transform = 'translate(0, -100%) rotate(45deg)';
+        document.body.style.overflowY = 'hidden';
         return;
     } else if (menu.style.transform === 'translateX(0px)') {
         menu.style.transform = 'translateX(100%)';
@@ -37,9 +105,10 @@ const toggleMenu = () => {
         toggleMenuBtnSpan[0].style.transform = 'translate(0, 0) rotate(0)';
         toggleMenuBtnSpan[1].style.transform = 'scale(1)'
         toggleMenuBtnSpan[2].style.transform = 'translate(0, 0) rotate(0)';
+        document.body.style.overflowY = 'scroll';
         return;
     }
-
+    document.body.style.overflowY = 'hidden';
     menu.style.transform = 'translateX(0)';
     menuShadow.style.display = 'block';
     toggleMenuBtn.style.justifyContent = 'center';
